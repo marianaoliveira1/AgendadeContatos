2c5302fa3971cfd09cfdda9daea0c42f36903ad0
Mariana Oliveira <marianaoliveirafernandes19@gmail.com>
2021-10-22 06:26:21 -0300

    calculadora

--- lib/helprs/contact_helper.dart
+++ lib/helprs/contact_helper.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
 import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
+final String contactTable = "contactTable";
 final String idColumn = "idColumin";
 final String nameColumn = "naameColumin";
 final String emailColumn = "emailColumin";
@@ -7,18 +11,44 @@ final String phoneColumn = "phoneColumin";
 final String imgColumn = "imgColumin";
 
 class ContactHelper {
+  static final ContactHelper _instance = ContactHelper.internal();
 
+  factory ContactHelper() => _instance;
+
+  ContactHelper.internal();
+
+  Database? _db;
+
+  Future<Database?> get db async{
+    if (_db != null) {
+      return _db;
+    } else {
+      _db = await initDb();
+      return _db;
+    }
+  }
+
+  Future<Database> initDb() async {
+    final dataBasePath = await getDatabasesPath();
+    final path = join(dataBasePath, "contacts.db");
+
+    return await openDatabase(path, version: 1,
+        onCreate: (Database db, int newerVersion) async {
+      await db.execute(
+          "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT"
+          "$phoneColumn TEXT, $imgColumn TEXT)");
+    });
+  }
 }
 
 class Contact {
-
   int? id;
   String? name;
   String? email;
   String? phone;
   String? img;
 
-  Contact.fromMap(Map map){
+  Contact.fromMap(Map map) {
     id = map[idColumn];
     name = map[nameColumn];
     email = map[emailColumn];
@@ -34,7 +64,7 @@ class Contact {
       imgColumn: img
     };
 
-    if(id !=null){
+    if (id != null) {
       map[idColumn] = id;
     }
     return map;
